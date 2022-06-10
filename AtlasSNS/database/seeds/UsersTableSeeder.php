c87b40b0a22debe32d3242e054baec3c69b1c7e2
cococo-f <fujikawa.k1130@gmail.com>
2022-06-10 11:18:14 +0900

    初期ユーザー作成（username,mail,passwordを記述）

--- AtlasSNS/database/seeds/UsersTableSeeder.php
+++ AtlasSNS/database/seeds/UsersTableSeeder.php
@@ -13,9 +13,19 @@ class UsersTableSeeder extends Seeder
     {
         //初期ユーザー作成//
         DB::table('users')->insert([
-            ['username' => 'おもち'],
-            ['username' => 'えび'],
-            ['username' => 'ぎょうざ']
+
+            ['username' => 'おもち',
+             'mail' => 'omochi@0815.com',
+             'password' => 'mochimochi'],
+
+            ['username' => 'ぎょうざ',
+             'mail' => 'gyouza@0704.com',
+             'password' => 'gyouzagyouza'],
+
+             ['username' => 'えび',
+             'mail' => 'ebi@1102.com',
+             'password' => 'ebiebi']
+
         ]);
 
     }
